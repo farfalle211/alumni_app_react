cc9774b957b9f3074409925cc63def0d76d360b5
farfalle211 <farfalle211@gmail.com>
2019-04-30 17:01:07 -0500

    begin to merge theme

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ import { Route,
 import axios from 'axios'
 import * as serviceWorker from './serviceWorker';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard.css?v=1.2.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
+
 import { Routes } from './Routes'
 
 axios.defaults.baseURL = 
--- src/views/TableList/TableList.jsx
+++ src/views/TableList/TableList.jsx
@@ -1,61 +1,10 @@
-// import React from 'react'
-// import Resource from '../components/Resource'
-// import axios from 'axios'
-// import ResourceCard from '../components/ResourceCard'
-// import Card from '../components/Card/Card'
-
-// class Resources extends React.Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       resources: []
-//     }
-//     // this.resources = this.resources.bind(this)
-//     // axios.get("/api/resources")
-//     //   .then(res => {
-//     //     const resources = res.data;
-//     //     console.log(res.data)
-//     //     this.setState({ resources });
-//     //   })
-//   }
-
-//   componentDidMount() {
-//     axios.get("/api/resources")
-//       .then(res => {
-//         const resources = res.data;
-//         console.log(res.data)
-//         this.setState({ resources });
-//       }) 
-//   }
-
-//   render() {
-//     // const resourceComponents = this.resources.map(pen => <Resource key={pen.id} resource={pen} />)
-//     return (
-//       <div>
-//         <h1>Hello</h1>
-//         <Card />
-//         <Card />
-//         <Card />
-//         <Card />
-//         <Card />
-//         <Card />
-        
-//       </div>
-//     )
-//   }
-// }
-// export default Resources
-
-//   // <Resource />
-//    // <ResourceCard />
-
 import React, { Component } from "react";
 import { Grid, Row, Col, Table } from "react-bootstrap";
 
 import Card from "components/Card/Card.jsx";
 import { thArray, tdArray } from "variables/Variables.jsx";
 
-class Resources extends Component {
+class TableList extends Component {
   render() {
     return (
       <div className="content">
@@ -130,4 +79,4 @@ class Resources extends Component {
   }
 }
 
-export default Resources;
+export default TableList;
