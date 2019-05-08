c0d05ada01aa39d281cb8e5292070bee9c4e950f
farfalle211 <farfalle211@gmail.com>
2019-05-08 16:42:56 -0500

    added login componnent w jwt

--- src/views/NewProfile.js
+++ src/views/NewProfile.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import UserForm from '../components/UserForm'
+import AddProject from '../components/AddProject'
 
 function NewProfile() {
   return(
     <div>
       <UserForm />
+      <br />
+      <AddProject />
     </div>
   )  
 }
