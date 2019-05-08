d47140ce58a75144bc06c02a64e562cf2e4474b1
pnb90 <phuocbui90@gmail.com>
2019-05-08 15:49:13 -0500

    created components viewer, made header and footer components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Resources from './views/Resources'
-import SkillsComponent from './components/SkillsComponent'
 
 class App extends React.Component {
   render() {
     return (
   <div>
     <h1>Home</h1>
-    <SkillsComponent  />
   </div>
       
     )
