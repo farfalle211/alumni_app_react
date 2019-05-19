f022e2c7cef0701cfc0f6f554f50921e61235baf
pnb90 <phuocbui90@gmail.com>
2019-05-19 12:53:18 -0500

    some nice css and colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ function Header() {
           <div className="container clearfix">         
               <img className="profile-image img-fluid float-right" src="../assets/images/ActualizeNew.png" alt="Actualize Logo" />
               <div className="profile-content float-left">
-                  <h1 className="name">Actualize Coding Bootcamp</h1>
-                  <h2 className="desc">Alumni Network</h2>   
+                  <h2 className="name">Actualize Coding Bootcamp</h2>
+                  <h3 className="desc">Alumni Network</h3>   
               </div>    
           </div>
       </header>
